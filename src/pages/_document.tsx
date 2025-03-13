a9b9960bc45f32484862cba0200daaea357b0687
Anil Karaabali <anilkaraabali@gmail.com>
2025-03-13 17:36:18 +0300

    feat: added pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,33 @@ export default class MyDocument extends Document<DocumentInitialProps> {
   render() {
     return (
       <Html lang='en' style={{ scrollBehavior: 'smooth' }}>
-        <Head />
+        <Head>
+          <link
+            href='/favicon/favicon-16x16.png'
+            rel='icon'
+            sizes='16x16'
+            type='image/png'
+          />
+          <link
+            href='/favicon/favicon-32x32.png'
+            rel='icon'
+            sizes='32x32'
+            type='image/png'
+          />
+          <link
+            href='/favicon/favicon.ico'
+            rel='shortcut icon'
+            sizes='16x16 32x32 48x48'
+            type='image/x-icon'
+          />
+          <link
+            href='/favicon/apple-touch-icon.png'
+            rel='apple-touch-icon'
+            sizes='180x180'
+            type='image/png'
+          />
+          <link href='/favicon/site.webmanifest' rel='manifest' />
+        </Head>
         <body
           className={clsx(
             'min-h-screen bg-background font-sans antialiased',
